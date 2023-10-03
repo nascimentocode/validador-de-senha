b4353cc9a3de66ef04d98ec165a13633dc0a4df9
João Vitor Nascimento da Silva <joaovitornascimento1885@gmail.com>
2023-10-03 17:35:49 -0300

    Implementada a validação da senha

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import generatePassword
 
-listPassword = generatePassword.main()
+if __name__ == "__main__":
+    listPassword = generatePassword.main()
 
-for password in listPassword:
-    if len(password) >= 8 and any(p.isupper() for p in password):
-        print(f"{password} -> Aprovado")
-    else:    
-        print(f"{password} -> Reprovado")
+    for password in listPassword:
+        # A senha valida se tem 8 ou mais digítos, se tem pelo menos uma letra maiúscula, um número e um caractere especial
+        if len(password) >= 8 and any(p.isupper() for p in password) and any(p.isalpha() for p in password) and any(p.isalnum() for p in password):
+            print(f"{password} -> Aprovado")
+        else:
+            print(f"{password} -> Reprovado")
